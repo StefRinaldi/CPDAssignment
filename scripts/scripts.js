5ceeebc65085c53672a85d8b93a0ea8daf912e34
Stefano Rinaldi <14480397@students.lincoln.ac.uk>
2017-03-02 00:07:26 +0000

    Added GeoLocation Using GPS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,33 @@
 // use when  in browser
 $(document).ready(function () {
+	
 	console.log('ready');
+
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
+
+	function success(pos) {
+		var crd = pos.coords;
+
+		localStorage.setItem("lat", crd.latitude);
+		localStorage.setItem("lng", crd.longitude);
+
+		console.log(localStorage.lat)
+
+		console.log('Your current position is:');
+		console.log(`Latitude : ${crd.latitude}`);
+		console.log(`Longitude: ${crd.longitude}`);
+		console.log(`More or less ${crd.accuracy} meters.`);
+	};
+
+	function error(err) {
+	  console.warn(`ERROR(${err.code}): ${err.message}`);
+	};
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
 
 	var lat = 53.2307;
 	var long = -0.5406;
@@ -15,14 +42,14 @@ $(document).ready(function () {
 			type: "GET",
 			crossDomain:true,
 			dataType: "json",
-			url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=500&type=bar&key=AIzaSyCzkA7RIl14ppr-tf6jBoPVDRuU7jBF_W0",
+			url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + localStorage.lat + "," + localStorage.lng + "&radius=500&type=bar&key=AIzaSyCzkA7RIl14ppr-tf6jBoPVDRuU7jBF_W0",
 			success: function(data){
 				console.log(data);
 				$("#pub-title-map").text(data.results[3].name);
 
 				map = new google.maps.Map(document.getElementById('map'), {
 					center: {lat: data.results[3].geometry.location.lat, lng: data.results[3].geometry.location.lng},
-					zoom: 15
+					zoom: 17
 				});
 			}
 		});
