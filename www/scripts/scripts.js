9cd124fb8c784bd432572907d9cb6a5be3328a33
anaon <anon@anon.co.uk>
2017-04-05 14:11:33 +0100

    yesh Boi

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -1,61 +1,174 @@
 // use when  in browser
 document.addEventListener("deviceready", onDeviceReady, false);
+
 function onDeviceReady() {
-var push = PushNotification.init({
-"android": {
-"senderID": "825647654839"
-},
-"ios": {
-"sound": true,
-"alert": true,
-"badge": true,
-"categories": {
-"invite": {
-"yes": {
-"callback": "app.accept", "title": "Accept", "foreground": true, "destructive": false
-},
-"no": {
-"callback": "app.reject", "title": "Reject", "foreground": true, "destructive": false
-},
-"maybe": {
-"callback": "app.maybe", "title": "Maybe", "foreground": true, "destructive": false
+	
+	var push = PushNotification.init({
+		"android": {
+			"senderID": "825647654839"
+		},
+		"ios": {
+			"sound": true,
+			"alert": true,
+			"badge": true,
+			"categories": {
+				"invite": {
+					"yes": {
+						"callback": "app.accept", "title": "Accept", "foreground": true, "destructive": false
+					},
+					"no": {
+						"callback": "app.reject", "title": "Reject", "foreground": true, "destructive": false
+					},
+					"maybe": {
+						"callback": "app.maybe", "title": "Maybe", "foreground": true, "destructive": false
+					}
+				},
+				"delete": {
+					"yes": {
+					"callback": "app.doDelete", "title": "Delete", "foreground": true, "destructive": true
+					},
+					"no": {
+						"callback": "app.cancel", "title": "Cancel", "foreground": true, "destructive": false
+					}
+				}
+			}
+		},
+		"windows": {}
+	});
+
+	push.on('registration', function(data) {	
+		console.log("registration event: " + data.registrationId);
+		var oldRegId = localStorage.getItem('registrationId');
+		if (oldRegId !== data.registrationId) {
+			// Save new registration ID
+			localStorage.setItem('registrationId', data.registrationId);
+			// Post registrationId to your app server as the value has changed
+		}
+	});
+
+	push.on('error', function(e) {
+		console.log("push error = " + e.message);
+	});
+
+	push.on('notification', function(data) {
+		console.log('notification event');
+		navigator.notification.alert(
+			data.message, // message
+			null, // callback
+			data.title, // title
+			'Ok' // buttonName
+		);
+	});
 }
-},
-"delete": {
-"yes": {
-"callback": "app.doDelete", "title": "Delete", "foreground": true, "destructive": true
-},
-"no": {
-"callback": "app.cancel", "title": "Cancel", "foreground": true, "destructive": false
-}
-}
-}
-},
-"windows": {}
+
+$(document).ready(function(){
+	get_location();
+	get_pub_data();
+	pick_pub();
+
+	if(localStorage.getItem("currentCrawl") == null){
+		localStorage.setItem("currentCrawl", JSON.stringify(currentCrawl));
+	} else {
+		var currentCrawl = JSON.parse(localStorage.getItem("currentCrawl"));
+
+		localStorage.setItem("currentCrawl", JSON.stringify(currentCrawl));
+	}
+
+	$('.reviews').slick({
+		infinite: true,
+		arrows: false
+	});
+	
+	$('#start-playing').click(function(){
+		$(':mobile-pagecontainer').pagecontainer('change', '#main-page', {
+			transition: 'slidedown',
+			changeHash: false
+		}, 5000);
+	});
+
+	$('#continue-old').click(function(){
+		$(':mobile-pagecontainer').pagecontainer('change', '#main-page', {
+			transition: 'slidedown',
+			changeHash: false
+		}, 5000);
+
+		localStorage.removeItem("currentCrawl");
+	});
+
+    $('.another').click(function(){
+		$(this).children("i").rotate({
+			angle:0,
+			animateTo:360
+		});
+
+		pick_pub();
+	});
+
+	$('#info-button').click(function(){
+		$('#info').slideToggle();
+
+		$('.reviews').get(0).slick.setPosition();
+		var text = $('#info-button').text();
+    	$('#info-button').text(
+        	text == "info" ? "cancel" : "info");
+	});
+
+	$('.drink').click(function(){
+		$(':mobile-pagecontainer').pagecontainer('change', '#pub-page', {
+			transition: 'slidedown',
+			changeHash: false
+		}, 5000);
+
+		var stuff = JSON.parse(localStorage.getItem("currentPub"));
+
+		$('#on-your-way span').text(stuff.result.name);
+		$('#directions-button').attr("href", "https://www.google.co.uk/maps/dir/" + localStorage.lat + "," + localStorage.lng + "/" + stuff.result.geometry.location.lat + "," + stuff.result.geometry.location.lng + "/");
+		$('#review-title').text(stuff.result.reviews["0"].author_name);
+		$('#review-content').text(stuff.result.reviews["0"].text);
+		$('#review-profile-img').html("<img src='" + stuff.result.reviews["0"].profile_photo_url + "'>");
+	});
+
+	$('#back').click(function(){
+		$(':mobile-pagecontainer').pagecontainer('change', '#main-page', {
+			transition: 'slidedown',
+			changeHash: false
+		}, 5000);
+	});
+
+	$('#done-button').click(function(){
+		$(':mobile-pagecontainer').pagecontainer('change', '#main-page', {
+			transition: 'slidedown',
+			changeHash: false
+		}, 5000);
+
+		var stuff = JSON.parse(localStorage.getItem("currentPub"));
+
+		var currTime = new Date().toLocaleString();
+
+		var newPub = [
+			stuff.result.name,
+			stuff.result.place_id,
+			currTime
+		]
+
+		console.log(newPub);
+		console.log(localStorage.getItem("currentCrawl"));
+
+		if(localStorage.getItem("currentCrawl") == null){
+			var currentCrawl = [];
+			currentCrawl.push(newPub);
+			localStorage.setItem("currentCrawl", JSON.stringify(currentCrawl));
+		} else {
+			var currentCrawl = JSON.parse(localStorage.getItem("currentCrawl"));
+			currentCrawl.push(newPub);
+			localStorage.setItem("currentCrawl", JSON.stringify(currentCrawl));
+		}
+
+		console.log(JSON.parse(localStorage.getItem("currentCrawl")));	
+	});
 });
-push.on('registration', function(data) {
-console.log("registration event: " + data.registrationId);
-var oldRegId = localStorage.getItem('registrationId');
-if (oldRegId !== data.registrationId) {
-// Save new registration ID
-localStorage.setItem('registrationId', data.registrationId);
-// Post registrationId to your app server as the value has changed
-}
-});
-push.on('error', function(e) {
-console.log("push error = " + e.message);
-});
-push.on('notification', function(data) {
-console.log('notification event');
-navigator.notification.alert(
-data.message, // message
-null, // callback
-data.title, // title
-'Ok' // buttonName
-);
-});
-}
-$(document).ready(function () {
+
+function get_location(){
 
 	var options = {
 		enableHighAccuracy: true,
@@ -85,109 +198,26 @@ $(document).ready(function () {
 	};
 
 	navigator.geolocation.getCurrentPosition(success, error, options);
-	
-	$('#start-playing').click(function(){
-		$(':mobile-pagecontainer').pagecontainer('change', '#main-page', {
-			transition: 'slidedown',
-			changeHash: false
-		}, 5000);
+}
 
-		$.ajax({ 
-			type: "GET",
-			crossDomain:true,
-			dataType: "json",
-			url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + localStorage.lat + "," + localStorage.lng + "&type=bar&rankby=distance&key=AIzaSyCzkA7RIl14ppr-tf6jBoPVDRuU7jBF_W0",
-			success: function(data){
-				console.log(data);
-				localStorage.setItem("placesData", JSON.stringify(data));
-				console.log(JSON.parse(localStorage.placesData));
-			}
-		});
+function get_pub_data(){
+	$.ajax({ 
+		type: "GET",
+		crossDomain:true,
+		dataType: "json",
+		url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + localStorage.lat + "," + localStorage.lng + "&type=bar&rankby=distance&key=AIzaSyCzkA7RIl14ppr-tf6jBoPVDRuU7jBF_W0",
+		success: function(data){
+			console.log(data);
+			localStorage.setItem("placesData", JSON.stringify(data));
+			console.log(JSON.parse(localStorage.placesData));
+		}
 
-		var stuff = JSON.parse(localStorage.getItem("placesData"));
-
-		console.log(stuff);
-		
-		var indexRnd = Math.floor(Math.random() * 20);
-
-		$("#pub-title-map").text(stuff.results[indexRnd].name);
-		
-		map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: stuff.results[indexRnd].geometry.location.lat, lng: stuff.results[indexRnd].geometry.location.lng},
-			zoom: 17
-		});
-
-		var myLatLng = {lat: stuff.results[indexRnd].geometry.location.lat, lng: stuff.results[indexRnd].geometry.location.lng};
-
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			animation: google.maps.Animation.DROP,
-			map: map
-		});
-
-		marker.setMap(map);
-
-		console.log(stuff.results[indexRnd].place_id);
-
-		$.ajax({ 
-			type: "GET",
-			crossDomain:true,
-			dataType: "json",
-			url: "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + stuff.results[indexRnd].place_id + "&key=AIzaSyCzkA7RIl14ppr-tf6jBoPVDRuU7jBF_W0",
-			success: function(data){
-				localStorage.setItem("daplace", JSON.stringify(data));
-				console.log(data.result);
-				$('#address').html(data.result.adr_address);
-				$("#address").html($("#address").html().replace(',',''));
-
-				$open = $('#open');
-
-				if(data.result.opening_hours){
-					$open.attr("data-open", data.result.opening_hours.open_now);
-				} else {
-					$open.attr("data-open", "unknown");
-				}
-				
-				if($open.attr("data-open") == "true"){
-					$open.text("Open");
-					$open.css({"background" : "#128c0e"});
-				} else if ($open.attr("data-open") == "false"){
-					$open.text("Closed");
-					$open.css({"background" : "#e01a23"});
-				} else if ($open.attr("data-open") == "unknown"){
-					$open.text("Unknown");
-					$open.css({"background" : "#e01a23"});
-				}
-
-				$('#website-link').attr("href", data.result.website);
-				$('#rating').text(data.result.rating);
-
-				console.log(data.result.name);
-
-				$('.reviews').slick('removeSlide', null, null, true);
-
-				$.each(data.result.reviews, function(key, value){
-					$('.reviews').slick('slickAdd', "<div class='review'><img src='" + value.profile_photo_url + "'/>" + value.text + "</div>");
-				});
-
-
-
-
-				
-			}
-		});
 
 	});
+}
 
-    $('.another').click(function(){
-
-
-		$(this).children("i").rotate({
-			angle:0,
-			animateTo:360
-		});
-
-    	$('#pub-title-map').slideUp(200).slideDown(200);
+function pick_pub() {
+	$('#pub-title-map').slideUp(200).slideDown(200);
 
 		var indexRnd = Math.floor(Math.random() * 20);
 
@@ -220,7 +250,7 @@ $(document).ready(function () {
 			dataType: "json",
 			url: "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + stuff.results[indexRnd].place_id + "&key=AIzaSyCzkA7RIl14ppr-tf6jBoPVDRuU7jBF_W0",
 			success: function(data){
-				localStorage.setItem("daplace", JSON.stringify(data));
+				localStorage.setItem("currentPub", JSON.stringify(data));
 				console.log(data.result);
 				$('#address').html(data.result.adr_address);
 				$("#address").html($("#address").html().replace(',',''));
@@ -254,48 +284,4 @@ $(document).ready(function () {
 				});
 			}
 		});
-
-	});
-
-	$('#info-button').click(function(){
-		$('#info').slideToggle();
-
-		$('.reviews').get(0).slick.setPosition();
-		var text = $('#info-button').text();
-    	$('#info-button').text(
-        	text == "info" ? "cancel" : "info");
-	});
-
-	$('.drink').click(function(){
-		$(':mobile-pagecontainer').pagecontainer('change', '#pub-page', {
-			transition: 'slidedown',
-			changeHash: false
-		}, 5000);
-
-		var stuff = JSON.parse(localStorage.getItem("daplace"));
-
-		console.log(stuff.result.geometry.location.lat);
-
-		$('#on-your-way span').text(stuff.result.name);
-		$('#directions-button').attr("href", "https://www.google.co.uk/maps/dir/" + localStorage.lat + "," + localStorage.lng + "/" + stuff.result.geometry.location.lat + "," + stuff.result.geometry.location.lng + "/");
-		$('#review-title').text(stuff.result.reviews["0"].author_name);
-		$('#review-content').text(stuff.result.reviews["0"].text);
-		$('#review-profile-img').html("<img src='" + stuff.result.reviews["0"].profile_photo_url + "'>");
-	});
-
-	$('#back').click(function(){
-		$(':mobile-pagecontainer').pagecontainer('change', '#main-page', {
-			transition: 'slidedown',
-			changeHash: false
-		}, 5000);
-	});
-});
-
-$(document).ready(function(){
-	$('.reviews').slick({
-		infinite: true,
-		arrows: false,
-		dots: true
-	});
-
-});
+}
